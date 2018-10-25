c50d8ce20e66eba06214a5467d562b8cce3fbd33
James Finkelstein <jfinkelstein9@me.com>
2018-10-24 22:00:54 -0400

    updated direction

--- script.js
+++ script.js
@@ -1,35 +1,56 @@
 
+    // var canvas = document.getElementById('canvas');
+    // var c = canvas.getContext('2d');
+
+    // var canvasTwo = document.getElementById('sunside');
+    // var c2 = canvasTwo.getContext('2d');
+
+    // //image variable
+    // var img = document.getElementById('egg');
+
+    // //pan canvas sizing
+    // canvas.width = 480;
+    // canvas.height = 320;
+    
+    // //egg canvas sizing
+    // canvasTwo.width = 100;
+    // canvasTwo.height = 100;
+
+    // //holder variable
+    // // let initialY = ;
+
+    // //function reads orientation change, then
+    // //redraw with egg image in new location
+    // window.addEventListener('deviceorientation', (e) => {
+        
+    //     console.log(currentPosition);
+    //     if (currentPosition > e.beta) {
+    //         canvasTwo.style.top = `${canvasTwo.getBoundingClientRect().top + e.beta}px`;
+    //         console.log('positive')
+    //     } else {
+    //         canvasTwo.style.top = `${canvasTwo.getBoundingClientRect().top - e.beta}px`;
+    //         console.log('negative')
+    //     }
+    //     currentPosition = e.beta;
+    // });
+
+    //
     var canvas = document.getElementById('canvas');
     var c = canvas.getContext('2d');
 
-    var canvasTwo = document.getElementById('sunside');
-    var c2 = canvasTwo.getContext('2d');
+    var img = new Image();
+    img.src = './images/egg.png';
 
-    //image variable
-    var img = document.getElementById('egg');
+    //
+    canvas.width = 320;
+    canvas.height = 480;
 
-    //pan canvas sizing
-    canvas.width = 480;
-    canvas.height = 320;
-    
-    //egg canvas sizing
-    canvasTwo.width = 100;
-    canvasTwo.height = 100;
+    c.beginPath();
+    c.drawImage(img, 130, 200, 80, 80);
 
-    //holder variable
-    let currentPosition = 0;
-
-    //function reads orientation change, then
-    //redraw with egg image in new location
     window.addEventListener('deviceorientation', (e) => {
-        
-        console.log(currentPosition);
-        if (currentPosition > e.beta) {
-            canvasTwo.style.top = `${canvasTwo.getBoundingClientRect().top + e.beta}px`;
-            console.log('positive')
-        } else {
-            canvasTwo.style.top = `${canvasTwo.getBoundingClientRect().top - e.beta}px`;
-            console.log('negative')
-        }
-        currentPosition = e.beta;
+        c.clearRect(0,0,canvas.width,canvas.height);
+        c.beginPath();
+        c.drawImage(img, 130 + e.alpha, 200 + e.beta, 80, 80);
     });
+    
